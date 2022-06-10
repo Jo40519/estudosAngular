f2abd72353d726ce2f49f6a369b806df52dc791a
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2022-06-10 08:44:17 -0300

    Vendo Event Binding

--- projetoEstudo/src/app/data-binding/data-binding.component.ts
+++ projetoEstudo/src/app/data-binding/data-binding.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-binding.component.css']
 })
 export class DataBindingComponent implements OnInit {
-
+  public valorAtual: string = '';
+  public valorSalvo: string =''
   constructor() { }
 
   ngOnInit() {
   }
 
+  botaoClicado() {
+    alert('Clicado')
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    console.log()
+    this.valorAtual = (<HTMLInputElement>evento.target).value
+  }
+
+  salvarValor(valor: string) {
+    this.valorSalvo = valor
+  }
 }
